c448237e2ffe8a1f8ba8b09c284205048f72a5fc
Pavel Mironchik <mironchikpavel@gmail.com>
2020-01-05 02:39:27 +0600

    Add simple configuration option.

---
+++ config/backup_panel.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backup Panel Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where Backup Panel will be accessible from.
+    | Feel free to change this path to anything you like. Note that the
+    | URI will not affect the paths of its internal API that aren't exposed
+    | to users.
+    |
+    */
+
+    'path' => 'backup'
+
+];
