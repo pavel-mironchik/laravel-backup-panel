c39d03e4cf6d815bd194e1469f2fd32d4efb3a74
Pavel Mironchik <mironchikpavel@gmail.com>
2020-01-05 00:18:31 +0600

    Dummy test.

---
+++ tests/Feature/DummyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace PavelMironchik\BackupPanel\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+class DummyTest extends TestCase
+{
+    /** @test */
+    public function dummyTest()
+    {
+        $this->assertTrue(true);
+    }
+}
