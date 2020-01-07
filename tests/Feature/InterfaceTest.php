b8763b0b4e631c3fdbf0a515bb76194fff015ef5
Pavel Mironchik <mironchikpavel@gmail.com>
2020-01-07 16:31:17 +0600

    Add some basic tests.

---
+++ tests/Feature/InterfaceTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace PavelMironchik\BackupPanel\Tests;
+
+use Illuminate\Foundation\Application;
+
+class InterfaceTest extends TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  Application  $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('app.key', 'base64:GhFMLyZ7x32kzu0How7wF8CIei+UC9Lc69Jcr+Z3sAk=');
+        $app['config']->set('app.name', 'Backup Panel Test');
+        $app['config']->set('backup_panel.path', 'backup_test');
+    }
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app->instance('path.public', __DIR__.'/../../public');
+    }
+
+    public function test_panel_is_served_at_configured_path()
+    {
+        $this->get('backup_test')
+            ->assertOk();
+    }
+
+    public function test_home_view_is_served()
+    {
+        $this->get('backup_test')
+            ->assertViewIs('backup_panel::layout');
+    }
+
+    public function test_home_view_gets_global_variables()
+    {
+        $this->get('backup_test')
+            ->assertViewHas('globalVariables', [
+                'appName' => 'Backup Panel Test',
+                'path' => 'backup_test'
+            ]);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PavelMironchik\BackupPanel\Tests;
+
+use Illuminate\Foundation\Application;
+use PavelMironchik\BackupPanel\BackupPanelServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            BackupPanelServiceProvider::class
+        ];
+    }
+}
