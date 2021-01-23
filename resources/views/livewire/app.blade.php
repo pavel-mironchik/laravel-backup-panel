455367899d1df79055b6055da5f6b9b327508fcc
Pavel Mironchik <mironchikpavel@gmail.com>
2021-01-24 01:25:48 +0600

    Remove blade templates.

--- resources/views/livewire/app.blade.php
+++ resources/views/livewire/app.blade.php
@@ -143,9 +143,13 @@
                             <td>{{ $backupStatus['disk'] }}</td>
                             <td>
                                 @if($backupStatus['healthy'])
-                                    @include('laravel_backup_panel::icons.healthy')
+                                    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" height="24px">
+                                        <path d="M2.93 17.07A10 10 0 1 0 17.07 2.93 10 10 0 0 0 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM4 10l2-2 3 3 5-5 2 2-7 7-5-5z" fill="var(--success)" fill-rule="evenodd"/>
+                                    </svg>
                                 @else
-                                    @include('laravel_backup_panel::icons.unhealthy')
+                                    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" height="24px">
+                                        <path d="M11.41 10l2.83-2.83-1.41-1.41L10 8.59 7.17 5.76 5.76 7.17 8.59 10l-2.83 2.83 1.41 1.41L10 11.41l2.83 2.83 1.41-1.41L11.41 10zm-8.48 7.07A10 10 0 1 0 17.07 2.93 10 10 0 0 0 2.93 17.07zm1.41-1.41A8 8 0 1 0 15.66 4.34 8 8 0 0 0 4.34 15.66z" fill="var(--danger)" fill-rule="evenodd"/>
+                                    </svg>
                                 @endif
                             </td>
                             <td>{{ $backupStatus['amount'] }}</td>
