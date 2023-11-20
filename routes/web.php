cf8b7334d543aa76c8e17dcb6edea50ccc66d1fb
Dimitar Yanakiev <dyanakiev@users.noreply.github.com>
2023-11-20 14:00:28 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-if(config('laravel_backup_panel.routes') === true) {
-  Route::view('/', 'laravel_backup_panel::layout')->name('laravel-backup-panel.index');
-}
+Route::view('/', 'laravel_backup_panel::layout')->name('laravel-backup-panel.index');
